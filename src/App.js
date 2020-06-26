e24a89e917da467bae4750b304bbb1b1749dc117
ahaanbutt <60073494+ahaanbutt@users.noreply.github.com>
2020-06-26 22:28:38 +0500

    Update App.js
    g

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  let [count, setcount ] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Value of counter variable is: {count}</h1>
+      <br />
+   
     </div>
   );
 }
-
-export default App;
